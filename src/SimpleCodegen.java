92d44a8775be4042afef278873cba68334d1a042
gaurav <gaurav.mahajan@students.iiit.ac.in>
2018-01-17 10:59:37 +0530

    Adding first file

---
+++ src/SimpleCodegen.java
@@ -0,0 +1,30 @@
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+
+public class SimpleCodegen {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		MethodSpec main = MethodSpec.methodBuilder("main")
+			    .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+			    .returns(void.class)
+			    .addParameter(String[].class, "args")
+			    .addStatement("$T.out.println($S)", System.class, "Hello, JavaPoet!")
+			    .build();
+
+			TypeSpec helloWorld = TypeSpec.classBuilder("HelloWorld")
+			    .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+			    .addMethod(main)
+			    .build();
+
+			JavaFile javaFile = JavaFile.builder("com.example.helloworld", helloWorld)
+			    .build();
+
+			javaFile.writeTo(System.out);
+
+	}
+
+}
