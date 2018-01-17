7a7851454612131cbabbdbac4f348cf93c45c147
gaurav <gaurav.mahajan@students.iiit.ac.in>
2018-01-17 18:37:50 +0530

    using jcodemodel

--- src/SimpleCodegen.java
+++ src/SimpleCodegen.java
@@ -3,10 +3,12 @@ import java.io.File;
 import com.squareup.javapoet.JavaFile;
 import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.TypeSpec;
+import com.sun.codemodel.ClassType;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JExpr;
 import com.sun.codemodel.JMethod;
+import com.sun.codemodel.JMod;
 
 import javax.lang.model.element.Modifier;
 
@@ -15,14 +17,14 @@ public class SimpleCodegen {
 	public static void main(String[] args) throws Exception{
 		// TODO Auto-generated method stub
 		JCodeModel cm = new JCodeModel();
-		JDefinedClass dc = cm._class("Bar");
-		JMethod m = dc.method(0, int.class, "foo");
-		m.body()._return(JExpr.lit(5));
+		JDefinedClass dc = cm._class("TestImplementation", ClassType.CLASS)._implements(Test.class);
+		JMethod m = dc.method(JMod.PUBLIC, int.class, "add");
+		m.param(int.class, "a");
+		m.param(int.class, "b");
 		
-		JMethod m2 = dc.method(0, int.class, "fun2");
-		m2.body()._return(JExpr.lit(5));
+		m.body()._return(JExpr.lit(0));
 		
-
+	
 		File file = new File("./src/");
 		file.mkdirs();
 		cm.build(file);
